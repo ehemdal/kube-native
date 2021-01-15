290969ee2075c1a32d80e318d904c39f15287062
ehemdal <erik.hemdal@gmail.com>
2021-01-15 20:50:55 +0000

    Forced a Java file change

--- api/src/main/java/com/docker/demo/DemoApp.java
+++ api/src/main/java/com/docker/demo/DemoApp.java
@@ -11,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.docker.demo.configuration.JpaConfiguration;
 
-
+/* Demo application */
 @Import(JpaConfiguration.class)
 @SpringBootApplication(scanBasePackages={"com.docker.demo"})
 @EntityScan("com.docker.demo.model")
